eb4d5e86df4084210743789d1075d9ce6cb62b78
rionaveen <naveenbelieve19@gmail.com>
2022-11-11 20:01:11 +0530

    pages

---
+++ LoginPages.java
@@ -0,0 +1,41 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPages {
+      WebDriver driver;
+
+	@FindBy(id="username")
+	private WebElement userName;
+	
+	@FindBy(id="password")
+	private WebElement passWord;
+	
+	@FindBy(id="login")
+	private WebElement loginBtn;
+	
+
+	public WebElement getUserName() {
+		return userName;
+	}
+
+	public WebElement getPassWord() {
+		return passWord;
+	}
+
+	public WebElement getLoginBtn() {
+		return loginBtn;
+	}
+	
+	public LoginPages(WebDriver driver) {
+		
+		this.driver=driver;
+		
+		PageFactory.initElements(driver, this);
+		
+	}
+
+}
